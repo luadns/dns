8a7d37134552b42f83a7068a3d461aa216624361
Vitalie Cherpec <vitalie@penguin.ro>
2012-04-06 11:12:17 +0300

    Sime Ramov: http://support.google.com/a/bin/answer.py?hl=en&answer=174125

--- templates.lua
+++ templates.lua
@@ -7,8 +7,8 @@ function google_app(domain)
   mx(domain, "aspmx.l.google.com", 5)
   mx(domain, "alt1.aspmx.l.google.com", 10)
   mx(domain, "alt2.aspmx.l.google.com", 10)
-  mx(domain, "aspmx2.l.google.com", 20)
-  mx(domain, "aspmx3.l.google.com", 20)
+  mx(domain, "aspmx2.l.googlemail.com", 20)
+  mx(domain, "aspmx3.l.googlemail.com", 20)
   
   -- Additional Google Apps records
   cname(concat("calendar", domain), "ghs.google.com")
