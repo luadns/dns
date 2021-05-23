fd2cd2c635d9c9dcf0db14b5e306139c4c3d656a
Vitalie Cherpec <vitalie@penguin.ro>
2021-05-23 19:40:45 +0300

    Apply new MX settings for Google Workspace

--- templates/google.lua
+++ templates/google.lua
@@ -1,10 +1,11 @@
 function google_app(domain)
-  -- Configure mail exchangers
-  mx(domain, "aspmx.l.google.com", 5)
-  mx(domain, "alt1.aspmx.l.google.com", 10)
-  mx(domain, "alt2.aspmx.l.google.com", 10)
-  mx(domain, "aspmx2.googlemail.com", 20)
-  mx(domain, "aspmx3.googlemail.com", 20)
+  -- Configure mail exchangers for Google Workspace, see:
+  -- https://support.google.com/a/answer/140034?hl=en
+  mx(domain, "aspmx.l.google.com", 1)
+  mx(domain, "alt1.aspmx.l.google.com", 5)
+  mx(domain, "alt2.aspmx.l.google.com", 5)
+  mx(domain, "alt3.aspmx.l.google.com", 10)
+  mx(domain, "alt4.aspmx.l.google.com", 10)
 
   -- Additional Google Apps records
   cname(concat("calendar", domain), "ghs.google.com")
